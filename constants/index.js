8bc2d47cf3a4ca97bfef9ba72fdc6f9bb7762e32
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-04-27 20:25:01 +0200

    setup

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+const { WhiteList } = require("./WhiteList");
+
+module.exports = {
+  FIGLET_NAME: ``,
+  CONTRACT_NAME: "SimpleNft",
+  TOKEN_NAME: "Simple Nft",
+  TOKEN_SYMBOL: "SNFT",
+  CONTRACT_CONTRACT_URI: "ipfs://test123456test.test/test123contractURI.json",
+  CONTRACT_BASE_URI: "ipfs://test123456test.test/",
+  CONTRACT_EXTENSION_URI: ".json",
+  MAX_SUPPLY: 1000,
+  MINT_PRICE: 0.5,
+  WhiteList: WhiteList,
+};
